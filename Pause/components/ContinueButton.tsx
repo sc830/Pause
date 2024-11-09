8a4fcdab0862649dbc687f3980e39532a0fc9e8d
Shelby Cumings <s_c830@txstate.edu>
2024-11-08 21:06:12 -0600

    Code we went over during our meeting 11/8

---
+++ Pause/components/ContinueButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Image, Text, StyleSheet, Pressable } from 'react-native';
+
+import settingsIcon from '../assets/images/settings.png';
+
+export default function ContinueButton(props: { onPress?: any; title?: String | undefined; }) {
+  const { onPress, title = '' } = props;
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Image source={settingsIcon} style={{ width: 50, height: 50}} resizeMode="contain"/>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 100,
+    borderRadius: 20,
+    elevation: 3,
+    height: 100,
+    width: 100,
+    margin: 0,
+    backgroundColor: '#4f7bbd',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
+
+
