cbc5e2235590b70896526afec23acd8c6ad0bfc9
jma352 <jma352@txstate.edu>
2024-11-11 17:13:40 -0600

    added diary and trophy png

--- Pause/app/logsPage.tsx
+++ Pause/app/logsPage.tsx
@@ -1,18 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import MenuButton from '@/components/MenuButton';
 
 export default function logsPage() {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-            <Text>This is the logs screen.</Text>
-            <MenuButton/>
-    </View>
-  );
+    <View style={styles.container}>
+    <MenuButton style={styles.menuButton} onPress={() => console.log('Menu Pressed')} />
+    {/* Other content for your screen */}
+  </View>
+);
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+menuButton: {
+  position: 'absolute',
+  top: 20,         // Distance from the top of the screen
+  right: 20,       // Distance from the right edge of the screen
+},
+});
--- Pause/components/MenuButton.tsx
+++ Pause/components/MenuButton.tsx
@@ -1,37 +1,27 @@
-import React from 'react';
-import { Image, Text, StyleSheet, Pressable } from 'react-native';
 
+import React from 'react';
+import { Image, StyleSheet, Pressable } from 'react-native';
 import settingsIcon from '../assets/images/menu.png';
 
-export default function menuButton(props: { onPress?: any; title?: String | undefined; }) {
-  const { onPress, title = '' } = props;
+export default function MenuButton(props: { onPress?: any; title?: String | undefined; style?: object }) {
+  const { onPress, style } = props;
   return (
-    <Pressable style={styles.button} onPress={onPress}>
-      <Image source={settingsIcon} style={{ width: 50, height: 50}} resizeMode="contain"/>
+    <Pressable style={[styles.button, style]} onPress={onPress}>
+      <Image source={settingsIcon} style={{ width: 50, height: 50 }} resizeMode="contain" />
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
-    paddingHorizontal: 100,
+    paddingHorizontal: 12,
     borderRadius: 20,
     elevation: 3,
-    height: 100,
-    width: 100,
-    margin: 0,
-    backgroundColor: '#4f7bbd',
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
+    height: 50,
+    width: 50,
+    //backgroundColor: '#4f7bbd',
   },
 });
-
-
