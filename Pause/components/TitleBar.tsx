385131387304b0e685e2b64c332cf62f1c2bb693
Shelby Cumings <s_c830@txstate.edu>
2024-11-12 21:33:55 -0600

    Title bar implementation

---
+++ Pause/components/TitleBar.tsx
@@ -0,0 +1,58 @@
+//const { width, height } = Dimensions.get('window');
+//import { useSafeAreaInsets } from 'react-native-safe-area-context';
+// style fixes
+
+import React, { useState } from 'react';
+import { View, SafeAreaView, Dimensions, StyleSheet, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import colors from '@/constants/Colors';
+import reusedStyles from '@/constants/reusedStyles';
+import values from '@/constants/Values';
+
+// types of expected props
+interface HeaderProps {
+  title: string;
+  color?: string;
+  textColor?: string;
+}
+  
+const TitleBar: React.FC<HeaderProps> = ({ 
+    title = "Page Title",
+    color = colors.yellow,                // default color yellow
+    textColor = colors.black,             // default text color black
+   }) => {
+
+  const { width, height } = Dimensions.get('window');
+  const insets = useSafeAreaInsets();     // Get safe area insets
+  const barHeight = height*.1 + insets.top;       // 10% screen height + safe area top inset
+  const barWidth = width * values.componentWidth;            // calculate width of component space
+  const leftPosition = (width*(1-values.componentWidth))/2;    // calculate left margin
+  
+  return (
+    <View style={[styles.titleBar, {backgroundColor: color, height:barHeight, width:barWidth}]}>
+      <SafeAreaView>
+        <Text style={[styles.titleText, {color: textColor, paddingTop: 8, marginBottom:12}]}>{title}</Text>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleBar: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderBottomLeftRadius: values.borderRadius,
+    borderBottomRightRadius: values.borderRadius,
+  },
+  titleText: {
+    alignItems: 'center',
+    letterSpacing: 1.5,
+    justifyContent: 'center',
+    fontSize: 36,
+    fontWeight: 100,
+  },
+});
+
+export default TitleBar;
