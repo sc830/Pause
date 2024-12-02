dfdf0ce896a1b2c3e35f36003f4dcddfbbc5fe6c
Shelby Cumings <s_c830@txstate.edu>
2024-12-01 19:28:48 -0600

    Error fix

--- Pause/app/login.tsx
+++ Pause/app/login.tsx
@@ -2,22 +2,22 @@
 
 
   References:
-    ChatGPT
-      Authentication/initializing template
-        const { user, initializing } = useAuth(); // Get user data from context
-        if (initializing) return <Text>Loading...</Text>; // Show loading until initialized
-        if (!user) {
-      Fix references to state variables
-        onChangeText={(text) => setState((prevState) => ({ ...prevState, email: text }))}
-        handleLoginSubmission(state.email, state.password)
+    Authentication/initializing template from ChatGPT
+      const { user, initializing } = useAuth(); // Get user data from context
+      if (initializing) return <Text>Loading...</Text>; // Show loading until initialized
+      if (!user) {
     React Native Firebase documentation https://rnfirebase.io/auth/usage#main
-    Login Screen example https://alitalhacoban.medium.com/basic-login-screen-with-react-native-c9f7fdcc8dae
+
+    onChangeText={(text) => setState((prevState) => ({ ...prevState, email: text }))}
+    
+    handleLoginSubmission(state.email, state.password)
 
     
 */
 
 import React, { useState } from 'react';
 import { Text, View } from "react-native";
+import { useRouter } from 'expo-router';
 import { signIn } from '@/constants/firebase'
 import { useAuth, AuthProvider } from '@/contexts/AuthContext';
 
@@ -30,13 +30,14 @@ import TextBox from '@/components/TextBox';
 
 import '@/constants/global.css';
 
-export default function Index() {
+export default function Login() {
 
     const [state,setState] = useState({
       email: '',
       password: '',
     })
 
+    const router = useRouter();
     const { user, initializing } = useAuth(); // Get user data from context
 
     if (initializing) return <Text>Loading...</Text>; // Show loading until initialized
@@ -66,8 +67,7 @@ export default function Index() {
                       buttonHeight={80}
                       buttonWidth={600}
                       onPress={() => {
-                          signIn(state.email, state.password)
-                          // add navigation to next page here
+                          handleLogin(state.email, state.password, router);
                       }}
                   />
                 </View>
@@ -76,7 +76,6 @@ export default function Index() {
         );
     }
     else {
-      console.log("Current user: ", user.email);
         return (
           <AuthProvider>
             <View
@@ -94,4 +93,9 @@ export default function Index() {
         )
     }
 };
+
+const handleLogin = async (email: string, pass: string, router: any) => {
+  await signIn(email, pass);
+  router.push('/');
+}
 
